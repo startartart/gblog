e1610639ef03d65f4cc57914ad4573dff9d47e4a
ParkByunggwon <startartart@naver.com>
2023-12-21 15:46:32 +0900

    chore: 배포 환경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import styled from 'styled-components';
+
+const GameList: Array<string> = ['가위바위보'];
+
 export default function App() {
-  return <>START</>;
+  const handleGoGame = (name: string) => {
+    console.log(name);
+  };
+
+  const games = GameList.map((game, idx) => (
+    <li key={idx} onClick={() => handleGoGame(game)}>
+      {game}
+    </li>
+  ));
+
+  return (
+    <>
+      <h4>게임 리스트</h4>
+      <ul>{games}</ul>
+    </>
+  );
 }
