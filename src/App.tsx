3ad16adcd9e02afde77285e069bdf95054ede23d
ParkByunggwon <startartart@naver.com>
2023-12-21 16:03:58 +0900

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 
-const GameList: Array<string> = ['가위바위보'];
+const indexList: Array<string> = ['공백1', '공백2'];
 
 export default function App() {
   const handleGoGame = (name: string) => {
     console.log(name);
   };
 
-  const games = GameList.map((game, idx) => (
-    <li key={idx} onClick={() => handleGoGame(game)}>
-      {game}
+  const index = indexList.map((index, idx) => (
+    <li key={idx} onClick={() => handleGoGame(index)}>
+      {index}
     </li>
   ));
 
   return (
     <>
-      <h4>게임 리스트</h4>
-      <ul>{games}</ul>
+      <h4>안내 바</h4>
+      <ul>{index}</ul>
     </>
   );
 }
