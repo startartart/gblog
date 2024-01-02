d70c7ab3593f90790ea13754c484486ec86a6aa4
ParkByunggwon <startartart@naver.com>
2024-01-02 23:20:21 +0900

    feat: SideText, SideBar 컴포넌트 구현

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,44 @@
+import { useState } from 'react';
 import styled from '@emotion/styled';
+import { projectData } from '../constants/projectData';
+import SideText from '../components/SideText';
 
-const SideBarLayout = styled.div<SideBarLayoutProps>`
+const SideBarLayout = styled.div`
   position: fixed;
-  width: 20%;
-  height: 80%;
+  width: 10%;
+  height: 20%;
   right: 0;
   top: 50%;
   transform: translateY(-50%);
-  background-color: black;
+  background-color: white;
+  border-radius: 1rem 0 0 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
 
-type SideBarLayoutProps = {
-  projects: number;
+type SideBarProps = {
+  current: string;
+  onClick: (id: number) => void;
 };
 
-export default function SideBar() {
-  return <SideBarLayout projects={3}></SideBarLayout>;
+export default function SideBar({ current, onClick }: SideBarProps) {
+  // const [fold, setFold] = useState(false);
+
+  return (
+    <SideBarLayout>
+      {projectData.map((project) => {
+        return (
+          <SideText
+            current={current === 'project' + project.id}
+            name={project.name}
+            id={project.id}
+            onClick={onClick}
+            key={project.id}
+          ></SideText>
+        );
+      })}
+    </SideBarLayout>
+  );
 }
