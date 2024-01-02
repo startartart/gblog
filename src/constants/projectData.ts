669118abe5424944ae69c92b96cde1a020282546
ParkByunggwon <startartart@naver.com>
2024-01-02 23:19:39 +0900

    chore: 프로젝트 데이터 추가

---
+++ src/constants/projectData.ts
@@ -0,0 +1,17 @@
+export const projectData = [
+  {
+    id: 1,
+    name: 'Project1',
+    color: '#dde6ed',
+  },
+  {
+    id: 2,
+    name: 'Project2',
+    color: '#dde6e2',
+  },
+  {
+    id: 3,
+    name: 'Project3',
+    color: '#dde6fd',
+  },
+];
