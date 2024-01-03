6953845e1b884aedcdd748e9754faef5181e6c68
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2024-01-03 22:47:33 +0900

    [Feat] 사이드바 접기/펴기
    * chore: 오른쪽화살표 추가
    
    * design: SideText 호버 및 TextBar 추가
    
    * feat: FoldBar 추가 및 접기/펴기 구현
    
    * fix: 가로 스크롤 제거

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,21 +2,49 @@ import { useState } from 'react';
 import styled from '@emotion/styled';
 import { projectData } from '../constants/projectData';
 import SideText from '../components/SideText';
+import RightArrow from '../assets/right-arrow.svg?react';
 
-const SideBarLayout = styled.div`
+const SideBarLayout = styled.div<SideBarLayoutProps>`
   position: fixed;
   width: 10%;
   height: 20%;
   right: 0;
   top: 50%;
-  transform: translateY(-50%);
+  transform: translateY(-50%)
+    ${(props) => (props.fold ? 'translateX(50%)' : '')};
   background-color: white;
   border-radius: 1rem 0 0 1rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
+  transition: transform 0.5s ease;
+
+  &:not(:first-of-type) {
+    transition: transform 0.5s ease;
+  }
+
+  ${(props) =>
+    props.fold &&
+    `
+    transform: translateX(70%) translateY(-50%);
+  `}
 `;
+
+const FoldBar = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 5%;
+  transform: translateY(-50%);
+  width: 10%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+type SideBarLayoutProps = {
+  fold: boolean;
+};
 
 type SideBarProps = {
   current: string;
@@ -24,10 +52,17 @@ type SideBarProps = {
 };
 
 export default function SideBar({ current, onClick }: SideBarProps) {
-  // const [fold, setFold] = useState(false);
+  const [fold, setFold] = useState(false);
+
+  const setFoldHandler = () => {
+    setFold(!fold);
+  };
 
   return (
-    <SideBarLayout>
+    <SideBarLayout fold={fold}>
+      <FoldBar onClick={setFoldHandler}>
+        <RightArrow height={'1rem'} width={'1rem'} />
+      </FoldBar>
       {projectData.map((project) => {
         return (
           <SideText
--- src/components/SideText.tsx
+++ src/components/SideText.tsx
@@ -1,8 +1,27 @@
+import { useState } from 'react';
 import styled from '@emotion/styled';
 
 const SideTextBarLayout = styled.div<SideTextBarBoxProps>`
   color: ${(props) => (props.current ? '#27374D' : '#000000')};
+  position: relative;
 `;
+
+const TextBar = styled.div`
+  position: absolute;
+  border-top: 3px;
+  border-top-style: solid;
+  width: 100%;
+  height: 100%;
+`;
+
+const Text = styled.p<TextProps>`
+  margin: 0;
+  color: ${(props) => (props.isHover ? '#750E21' : 'inherit')};
+`;
+
+type TextProps = {
+  isHover: boolean;
+};
 
 type SideTextBarProps = {
   current: boolean;
@@ -20,9 +39,16 @@ export default function SideTextBar({
   id,
   onClick,
 }: SideTextBarProps) {
+  const [isHover, setIsHover] = useState(false);
   return (
-    <SideTextBarLayout current={current} onClick={() => onClick(id)}>
-      {name}
+    <SideTextBarLayout
+      current={current}
+      onClick={() => onClick(id)}
+      onMouseOver={() => setIsHover(true)}
+      onMouseOut={() => setIsHover(false)}
+    >
+      <Text isHover={isHover}>{name}</Text>
+      {current && <TextBar />}
     </SideTextBarLayout>
   );
 }
