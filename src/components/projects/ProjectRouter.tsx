8701a18a90c46af08885167558be4988da9468f5
ParkByunggwon <startartart@naver.com>
2024-01-06 01:27:12 +0900

    feat: ProjectRouter 이하 옵션 추가

--- src/components/projects/ProjectRouter.tsx
+++ src/components/projects/ProjectRouter.tsx
@@ -3,13 +3,16 @@ import Text from './Text';
 
 type RouterProps = {
   type: string;
+  content: {
+    [key: string]: string;
+  }
 };
-export default function ProjectRouter({ type }: RouterProps) {
+export default function ProjectRouter({ type, content }: RouterProps) {
   switch (type) {
     case 'image':
-      return <Image />;
+      return <Image data={content.data} size={content.size} />;
     case 'text':
-      return <Text />;
+      return <Text data={content.data} fontColor={content.fontColor} />;
     default:
       return null;
   }
